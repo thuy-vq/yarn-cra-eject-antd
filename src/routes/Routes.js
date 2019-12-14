ebd7a8f86e9914e1875051a34851d28d6e552650
thuy-vq <thuy.vq@hotmail.com>
2019-12-14 10:53:46 +0700

    add Loadable

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Apps from '../pages/Apps/Apps';
+import Apps from '../pages/Apps/Loadable';
 import { Switch, HashRouter as Router, Route } from 'react-router-dom';
 import SagasExample from '../components/sagas-example/SagasExample';
 
@@ -9,9 +9,9 @@ const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Apps} />
         <Route path="/forums" component={Forum} />
         <Route path="/sagas" component={SagasExample} />
+        <Route path="/" component={Apps} />
       </Switch>
     </Router>
 
